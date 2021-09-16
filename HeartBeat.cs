f085d53281d5098347297954967e9a01111d70ca
Mariusz Noworyta <mariusz.noworyta@fenergo.com>
2021-09-16 23:46:27 +0200

    Windows service

---
+++ HeartBeat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace SimpleHeartBeatService
+{
+    public class HeartBeat
+    {
+        private readonly Timer _timer;
+
+        public HeartBeat()
+        {
+            _timer = new Timer(1000) { AutoReset=true};
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] text = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines("Heartbeat_trace.txt", text);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+
+namespace SimpleHeartBeatService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TopshelfExitCode exitCode = HostFactory.Run(x =>
+            {
+                x.Service<HeartBeat>(s =>
+                {
+                    s.ConstructUsing(hb => new HeartBeat());
+                    s.WhenStarted(hb => hb.Start());
+                    s.WhenStopped(hb => hb.Stop());
+                });
+
+                x.RunAsLocalSystem();
+                x.SetServiceName("HearBeat");
+                x.SetDisplayName("HeartBeat Service");
+            }
+            );
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, typeof(TopshelfExitCode));
+            Environment.ExitCode = exitCodeValue;
+
+
+        }
+    }
+}
